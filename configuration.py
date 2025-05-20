e8bc4d70b5c193ec7e47f3c5d568703148c4ce1a
Ahmed Jose Medina Ortiz <enzof420@gmail.com>
2025-05-20 16:24:27 -0600

    Mi primer commit

---
+++ configuration.py
@@ -0,0 +1,6 @@
+URL_SERVICE = "https://cnt-a59dd3f0-ea97-4848-aa60-38584903100a.containerhub.tripleten-services.com"
+DOC_PATH = "/docs/"
+LOG_MAIN_PATH = "/api/logs/main/"
+USERS_TABLE_PATH = "/api/db/resources/user_model.csv"
+CREATE_USER_PATH = "/api/v1/users/"
+PRODUCTS_KITS_PATH = "/api/v1/products/kits/"
---
+++ sender_stand_request.py
@@ -0,0 +1,21 @@
+import configuration
+import requests
+import data
+
+def post_new_user(user_body):
+    return requests.post(configuration.URL_SERVICE + configuration.CREATE_USER_PATH,  # inserta la dirección URL completa
+                         json=user_body,  # inserta el cuerpo de solicitud
+                         headers=data.headers)  # inserta los encabezados
+
+# esta función cambia los valores en el parámetro "firstName"
+def get_user_body(first_name):
+    # el diccionario que contiene el cuerpo de solicitud se copia del archivo "data" (datos) para conservar los datos del diccionario de origen
+    current_body = data.user_body.copy()
+    # Se cambia el valor del parámetro firstName
+    current_body["firstName"] = first_name
+    # Se devuelve un nuevo diccionario con el valor firstName requerido
+    return current_body
+
+def get_users_table():
+    return requests.get(configuration.URL_SERVICE + configuration.USERS_TABLE_PATH)
+
